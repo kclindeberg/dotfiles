ae2a97bec734c5c3a0255d109a73bbfb28fade0e
kclindeberg <k.c.lindeberg@gmail.com>
2017-02-05 21:08:31 +0100

    git: Set some global options

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+echo "Installing dotfiles"
+IFS='='
+for file in \
+	git=.config/git
+do
+	set $file
+	src="$PWD/$1"
+	dst="$HOME/$2"
+	if [ -L "$dst" ]; then
+		echo "- $dst: $(readlink $dst)"
+	else
+		if [ -e "$dst" ]; then
+			mv "$dst" "$dst~"
+		else
+			mkdir -p "${dst%/*}"
+		fi
+		ln -s "$src" "$dst"
+		echo "- $dst --> $src"
+	fi
+done
